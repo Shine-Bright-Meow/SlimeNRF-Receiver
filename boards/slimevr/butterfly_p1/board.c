161a684aa5d5dbd35fb7ae3850448c898019229d
ShineBrightMeow <142170650+ShineBrightMeow@users.noreply.github.com>
2025-08-13 18:18:57 -0700

    Update board.c

--- boards/slimevr/butterfly_p1/board.c
+++ boards/slimevr/butterfly_p1/board.c
@@ -4,33 +4,47 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/*
+ * Copyright (c) 2024 Your Company
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/init.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
-#include <hal/nrf_gpio.h>
+#include <zephyr/logging/log.h>
 
-static int board_early_init(void)
+LOG_MODULE_REGISTER(board, LOG_LEVEL_INF);
+
+#if DT_NODE_HAS_STATUS(DT_ALIAS(led0), okay)
+
+static int board_led_init(void)
 {
-    nrf_gpio_cfg_output(NRF_GPIO_PIN_MAP(0, 3));
-    nrf_gpio_pin_set(NRF_GPIO_PIN_MAP(0, 3));  // Set HIGH (LED OFF)
-    
-    return 0;
+	const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+	int ret;
+
+	if (!device_is_ready(led.port)) {
+		LOG_ERR("LED GPIO device not ready");
+		return -ENODEV;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Failed to configure LED pin: %d", ret);
+		return ret;
+	}
+
+	ret = gpio_pin_set_dt(&led, 0);
+	if (ret < 0) {
+		LOG_ERR("Failed to set LED pin state: %d", ret);
+		return ret;
+	}
+
+	LOG_DBG("LED initialized to OFF state");
+
+	return 0;
 }
 
-SYS_INIT(board_early_init, PRE_KERNEL_1, 0);
+SYS_INIT(board_led_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
 
-static int board_gpio_init(void)
-{
-    const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-    
-    if (!device_is_ready(led.port)) {
-        return -ENODEV;
-    }
-    
-    gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_set_dt(&led, 0);  // Ensure it stays OFF
-    
-    return 0;
-}
-
-SYS_INIT(board_gpio_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+#endif
