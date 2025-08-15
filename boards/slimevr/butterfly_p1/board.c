32177669b383f7c8965503f8565bc063cba79b85
ShineBrightMeow <142170650+ShineBrightMeow@users.noreply.github.com>
2025-08-14 18:41:22 -0700

    Update board.c

--- boards/slimevr/butterfly_p1/board.c
+++ boards/slimevr/butterfly_p1/board.c
@@ -4,47 +4,45 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
- * Copyright (c) 2024 Your Company
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/init.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pwm.h>
 #include <zephyr/logging/log.h>
+#include <hal/nrf_gpio.h>
 
 LOG_MODULE_REGISTER(board, LOG_LEVEL_INF);
 
-#if DT_NODE_HAS_STATUS(DT_ALIAS(led0), okay)
-
-static int board_led_init(void)
+static int board_early_init(void)
 {
-	const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-	int ret;
-
-	if (!device_is_ready(led.port)) {
-		LOG_ERR("LED GPIO device not ready");
-		return -ENODEV;
-	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
-	if (ret < 0) {
-		LOG_ERR("Failed to configure LED pin: %d", ret);
-		return ret;
-	}
-
-	ret = gpio_pin_set_dt(&led, 0);
-	if (ret < 0) {
-		LOG_ERR("Failed to set LED pin state: %d", ret);
-		return ret;
-	}
-
-	LOG_DBG("LED initialized to OFF state");
-
+	nrf_gpio_cfg_output(NRF_GPIO_PIN_MAP(0, 3));
+	nrf_gpio_pin_set(NRF_GPIO_PIN_MAP(0, 3));
+	
 	return 0;
 }
 
-SYS_INIT(board_led_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+SYS_INIT(board_early_init, PRE_KERNEL_1, 0);
 
-#endif
+static int board_pwm_init(void)
+{
+	const struct device *pwm_dev;
+	int ret;
+
+	pwm_dev = DEVICE_DT_GET(DT_NODELABEL(pwm0));
+	if (!device_is_ready(pwm_dev)) {
+		LOG_WRN("PWM device not ready yet");
+		return -ENODEV;
+	}
+
+	ret = pwm_set(pwm_dev, 0, PWM_MSEC(20), PWM_MSEC(20));
+	if (ret) {
+		LOG_ERR("Failed to set PWM to OFF state: %d", ret);
+		return ret;
+	}
+
+	LOG_INF("PWM initialized with LED in OFF state");
+	
+	return 0;
+}
+
+SYS_INIT(board_pwm_init, POST_KERNEL, 99);
